700100911436fba23f0982e6a833699e7e087c5b
ZacPearce <149570265+ZacPearce@users.noreply.github.com>
2025-04-06 16:28:35 +0100

    Created teams page, made a template to review teams added to admin

---
+++ tracker/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views 
+
+
+urlpatterns = [
+        path('teams/', views.team_list, name='team-list'),
+]
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Team
 # Create your views here.
+
+def team_list(request):
+    teams = Team.objects.all()
+    return render(request, 'tracker/team_list.html', {'teams': teams})
