0a4017369a816ab1c39f7e5ecfbcd60c4b2dbf9d
ZacPearce <149570265+ZacPearce@users.noreply.github.com>
2025-04-06 18:26:38 +0100

    migration to postgresql

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'Kendrick18',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
