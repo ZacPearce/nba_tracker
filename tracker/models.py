7bcc227843df27bf51424e064ff1e5a4a8df4dbb
ZacPearce <149570265+ZacPearce@users.noreply.github.com>
2025-04-06 15:54:03 +0100

    created models teams and players

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
