90b9cf5e7712eab421b2ec2c986c449053daa823
ZacPearce <149570265+ZacPearce@users.noreply.github.com>
2025-04-06 19:32:38 +0100

    fix: add dj-database url and procfile

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)%o)3^zx6w_v(2kw9xo%3%d+usw_7jo@gtp27648bhemj9b2-!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
